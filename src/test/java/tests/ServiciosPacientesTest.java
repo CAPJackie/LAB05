a6fdc940b9283c11160af9dd2d911816961c9f87
Juan David <juan.ramirez-me@mail.escuelaing.edu.co>
2017-09-21 12:31:43 -0500

    Test del metodo de registro de consulta de pacientes

--- src/test/java/tests/ServiciosPacientesTest.java
+++ src/test/java/tests/ServiciosPacientesTest.java
@@ -5,6 +5,12 @@
  */
 package tests;
 
+import edu.eci.pdsw.samples.entities.Consulta;
+import edu.eci.pdsw.samples.entities.Paciente;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosPacientes;
+import edu.eci.pdsw.samples.services.ServiciosPacientes;
+import edu.eci.pdsw.samples.services.impl.ServiciosPacientesMock;
+import java.util.Date;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -13,13 +19,42 @@ import static org.junit.Assert.*;
  *
  * @author hcadavid
  */
+
+
+/**
+ * Diseño de pruebas:
+ * 
+ * Clases de equivalencia:
+ *  
+ *      CE1: Descripción. El paciente no existe
+ *           Resultado esperado: No agrega la consulta.
+ *          
+ *      CE2: Descripción. Se agrega una consulta de un paciente
+ *           Resultado esperado: La lista de consultas del paciente debe aumentar
+ * 
+ */
 public class ServiciosPacientesTest {
+    
     
     public ServiciosPacientesTest() {
     }
     
     @Before
     public void setUp() {
+    }
+    
+    @Test
+    public void deberiaAgregarUnaConsultaAlPaciente() throws ExcepcionServiciosPacientes{
+        Paciente pac1 = new Paciente();
+        pac1.setId(10203040);
+        pac1.setTipoId("Cedula de ciudadania");
+        Consulta consulta = new Consulta(new Date("2017/09/21 12:22 PM"), "Chequeo general", 23000 );
+        ServiciosPacientes servPac = new ServiciosPacientesMock();
+        servPac.registrarNuevoPaciente(pac1);
+        servPac.agregarConsultaPaciente(10203040, "Cedula de ciudadania", consulta);
+        
+        assertEquals(1, pac1.getConsultas().size());
+        
     }
     
 }
